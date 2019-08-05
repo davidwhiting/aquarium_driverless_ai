8dcb438e9c58166474b8c68fdd0cd3ca0d336627
David Whiting <david.whiting@h2o.ai>
2019-08-05 12:49:19 -0600

    removed http from rest.sh

--- scoring_demo/card/rest.sh
+++ scoring_demo/card/rest.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 if [ $# -lt 2 ] ; then
-    echo "Usage: ./card_rest.sh URL MODEL_ID"
+    echo "Usage: ./rest.sh URL MODEL_ID"
+    echo ""
     exit 1
 
 else
 
-	URL=$1
-	model=$2
+    URL=$1
+    model=$2
 
-	echo ""
-	curl \
-		-d @card_data.json \
-		-X POST \
-		http://${URL}:8080/models/${model}/score \
-		-H "Content-Type: application/json"
-	echo ""
-	echo ""
+    echo ""
+    curl \
+        -d @card_data.json \
+        -X POST \
+        ${URL}:8080/models/${model}/score \
+        -H "Content-Type: application/json"
+    echo ""
 
-	exit 0
+    exit 0
 fi
